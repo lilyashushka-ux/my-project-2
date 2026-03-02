d05990ed67bb6563842d7b7f27a5be4ec910f8f5
Liliya <lilyashushka@gmail.com>
2026-03-02 13:55:19 +0200

    Add NavBar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,59 @@
 import React from "react";
+import { Col, Container, Nav, NavItem, TabContainer, TabContent, TabPane, NavLink, Row } from "react-bootstrap";
+
 
 export default class About extends React.Component {
     render() {
         return(
-            <div className="about">
-                <h1>About Us</h1>
-                <p>This is the about page of our React application. Here you can learn more about our company, our mission, and what we do. We are committed to providing you with the best experience possible, so feel free to explore and let us know if you have any questions or feedback.</p>
-            </div>
+        <Container>
+          <TabContainer id="left-tabs-example" defaultActiveKey="First">
+            <Row>
+          <Col sm={3}>
+          <Nav variant="pills" className="flex-column mt-2">
+          <NavItem>
+            <NavLink eventKey="First">Our Values</NavLink>
+          </NavItem>
+           <NavItem>
+            <NavLink eventKey="Second">Our Champions</NavLink>
+          </NavItem>
+           <NavItem>
+            <NavLink eventKey="Third">Our Puppiies</NavLink>
+          </NavItem>
+           <NavItem>
+            <NavLink eventKey="Fourth">Our Condition</NavLink>
+          </NavItem>
+           <NavItem>
+            <NavLink eventKey="Fifth">Our History</NavLink>
+          </NavItem>
+           </Nav>
+          </Col>
+          <Col sm={9}>
+          <TabContent className="mt-3">
+            <TabPane eventKey="First">
+          <img src="" alt="" />
+          <p></p>
+            </TabPane>
+              <TabPane eventKey="Second">
+          <img src="" alt="" />
+          <p></p>
+            </TabPane>
+              <TabPane eventKey="Third">
+          <img src="" alt="" />
+          <p></p>
+            </TabPane>
+              <TabPane eventKey="Fourth">
+          <img src="" alt="" />
+          <p></p>
+            </TabPane>
+              <TabPane eventKey="Fifth">
+          <img src="" alt="" />
+          <p></p>
+            </TabPane>
+          </TabContent>
+           </Col>
+            </Row>
+          </TabContainer>
+        </Container>
     )
 }
 }
