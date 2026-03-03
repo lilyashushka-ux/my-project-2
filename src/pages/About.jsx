2147f0b6cbcb74e8a9d8f441d0b3b5ce7ea0d5d4
Liliya <lilyashushka@gmail.com>
2026-03-03 20:39:17 +0200

    Add values card

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { Container, Row, Col, Tab, Nav, Card } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import puppy from "../assets/puppy.jpg";
 import puppy2 from "../assets/puppy-2.jpg";
 import puppy3 from "../assets/puppy-3.jpg";
+import value from "../assets/value-2.jpg";
+import value1 from "../assets/value-3.jpg";
+import value2 from "../assets/value-4.jpg";
+import value3 from "../assets/value-5.jpg";
 
 export default class About extends React.Component {
     render() {
@@ -33,8 +37,53 @@ export default class About extends React.Component {
           <Col sm={9}>
           <Tab.Content className="mt-3">
             <Tab.Pane eventKey="First">
+              <Container className="mt-4">
+  <Row className="g-4">
+
+    <Col md={6}>
+      <Card>
+        <Card.Img variant="top" src={value} width={500} height={300} />
+        <Card.Body>
+          <Card.Text>
+           From the very first weeks of life, our puppies are raised in close contact with people. They grow up surrounded by care, attention, and daily interaction, which helps them develop trust, confidence, and emotional stability. We gently introduce them to new experiences, voices, and environments to build a healthy and secure attachment.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+      <Col md={6}>
+      <Card className="h-100 shadow-sm">
+        <Card.Img variant="top" src={value1} width={500} height={300} />
+        <Card.Body>
+          <Card.Text>
+     At our kennel, training is not just an option — it is an essential part of raising a balanced and confident Border Collie. This breed is known for its exceptional intelligence, quick learning ability, and strong desire to work alongside its owner. Without proper mental stimulation, a Border Collie cannot fully thrive.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+     <Col md={6}>
+      <Card className="h-100 shadow-sm">
+        <Card.Img variant="top" src={value2} width={500} height={300} />
+        <Card.Body>
+          <Card.Text>
+    We believe that positive early exposure helps prevent fear and anxiety later in life. A well-socialized Border Collie grows into a balanced dog that adapts easily, remains calm in new situations, and builds healthy relationships with both humans and other animals
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+    <Col md={6}>
+      <Card className="h-100 shadow-sm">
+        <Card.Img variant="top" src={value3} width={500} height={300} />
+        <Card.Body>
+          <Card.Text>
+  Proper nutrition is the foundation of a puppy’s healthy growth and long-term well-being. In our kennel, we pay special attention to providing balanced, high-quality food that supports strong bones, healthy joints, a shiny coat, and stable digestion.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+       </Row>
+</Container>
           
-          <p>Values</p>
+          
             </Tab.Pane>
               <Tab.Pane eventKey="Second">
         
