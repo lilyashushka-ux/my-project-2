56af4877997777a57c6c729fc194d26c4de7c717
Liliya <lilyashushka@gmail.com>
2026-03-04 21:51:29 +0200

    Add condition block

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -48,6 +48,7 @@ export default class About extends React.Component {
       <Card>
         <Card.Img variant="top" src={value} width={500} height={300} />
         <Card.Body>
+          <Card.Title>Contact</Card.Title>
           <Card.Text>
            From the very first weeks of life, our puppies are raised in close contact with people. They grow up surrounded by care, attention, and daily interaction, which helps them develop trust, confidence, and emotional stability. We gently introduce them to new experiences, voices, and environments to build a healthy and secure attachment.
           </Card.Text>
@@ -58,6 +59,7 @@ export default class About extends React.Component {
       <Card className="h-100 shadow-sm">
         <Card.Img variant="top" src={value1} width={500} height={300} />
         <Card.Body>
+          <Card.Title>Training</Card.Title>
           <Card.Text>
      At our kennel, training is not just an option — it is an essential part of raising a balanced and confident Border Collie. This breed is known for its exceptional intelligence, quick learning ability, and strong desire to work alongside its owner. Without proper mental stimulation, a Border Collie cannot fully thrive.
           </Card.Text>
@@ -68,6 +70,7 @@ export default class About extends React.Component {
       <Card className="h-100 shadow-sm">
         <Card.Img variant="top" src={value2} width={500} height={300} />
         <Card.Body>
+        <Card.Title>Socialization</Card.Title>
           <Card.Text>
     We believe that positive early exposure helps prevent fear and anxiety later in life. A well-socialized Border Collie grows into a balanced dog that adapts easily, remains calm in new situations, and builds healthy relationships with both humans and other animals
           </Card.Text>
@@ -78,6 +81,7 @@ export default class About extends React.Component {
       <Card className="h-100 shadow-sm">
         <Card.Img variant="top" src={value3} width={500} height={300} />
         <Card.Body>
+          <Card.Title>Nutrition</Card.Title>
           <Card.Text>
   Proper nutrition is the foundation of a puppy’s healthy growth and long-term well-being. In our kennel, we pay special attention to providing balanced, high-quality food that supports strong bones, healthy joints, a shiny coat, and stable digestion.
           </Card.Text>
@@ -182,7 +186,7 @@ export default class About extends React.Component {
            <Col md={5}>
             <Card.Body>
               <Card.Title>All of our puppies</Card.Title>
-              <Card.Text>
+             
                  <ul className="custom-list list-unstyled ps-0">
       <li className="d-flex align-items-center gap-2 mt-2">
         <img src={checkIcon} alt="check" className="list-icon" width={17} height={17}/>
@@ -201,8 +205,7 @@ export default class About extends React.Component {
         <span>Begin early socialization from the first weeks of life</span>
       </li>
     </ul>
-              </Card.Text>
-            </Card.Body>
+      </Card.Body>
           </Col>
             <Col md={7}>
             <Card.Img
@@ -233,30 +236,102 @@ export default class About extends React.Component {
         </Row>
       </Card>
     </Col>
-
   </Row>
-</Container>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          
+</Container>    
             </Tab.Pane>
               <Tab.Pane eventKey="Fourth">
+                <Container>
+                  <Card>
+                    <Card.Body>
+                    <h2>Purchase Conditions</h2>
+                    <Card.Text>Bringing a Border Collie into your life is a long-term commitment. This intelligent and energetic breed requires time, patience, training, and active involvement from its owner. Our goal is not only to place puppies in loving homes, but to ensure they grow into healthy, balanced, and happy dogs.</Card.Text>
+                   
+                      <h5>Reservation</h5>
+                 <ul className="custom-list list-unstyled ps-0">
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17}/>
+        <span>Receive regular veterinary check-ups</span>
+      </li>
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Are vaccinated according to their age</span>
+      </li>
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Are raised in a loving, family environment</span>
+      </li>
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Begin early socialization from the first weeks of life</span>
+      </li>
+    </ul>
+              <Card.Title>What is included</Card.Title>
+                               <ul className="custom-list list-unstyled ps-0">
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17}/>
+        <span>Age-appropriate vaccinations</span>
+      </li>
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Regular veterinary check-ups</span>
+      </li>
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Microchip identification</span>
+      </li>
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Pedigree documents</span>
+      </li>
+          <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Veterinary passport</span>
+      </li>
+          <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Sales contract</span>
+      </li>
+          <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Starter puppy pack (food for the first days, familiar toy or blanket, care instructions)</span>
+      </li>
+    </ul>
+   <Card.Title> Age of transfer</Card.Title>
+    <Card.Text>
+      Puppies leave our kennel no earlier than 8–10 weeks of age. By this time they are socially adapted, emotionally stable, and ready to transition into their new family.
+    </Card.Text>
+      <h5>Owner responsibilities</h5>
+                                 <ul className="custom-list list-unstyled ps-0">
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17}/>
+        <span>Provide proper nutrition and veterinary care</span>
+      </li>
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Ensure daily physical activity and mental stimulation</span>
+      </li>
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Continue socialization and training</span>
+      </li>
+      <li className="d-flex align-items-center gap-2 mt-2">
+        <img src={checkIcon} alt="check" className="list-icon" width={17} height={17} />
+        <span>Offer a safe, loving, and responsible home</span>
+      </li>
+    </ul>
+    <Card.Title>Breeding and show rights</Card.Title>
+    <Card.Text>
+      Breeding rights are discussed individually. Not all puppies are sold for breeding purposes. Show-quality puppies are selected carefully based on structure, temperament, and breed standards.
+       </Card.Text>
+        <Card.Title>Our support</Card.Title>
+       <Card.Text>
+      We remain available for guidance and support throughout your dog’s life. We are always happy to answer questions regarding training, nutrition, or development.
+   </Card.Text>
+    </Card.Body>
+                  </Card>
+                </Container>
           
-          <p>Our Condition</p>
+         
             </Tab.Pane>
                <Tab.Pane eventKey="Fifth">
           
