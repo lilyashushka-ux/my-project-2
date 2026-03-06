eb35f78be443cf6001877d2b9cb5144b23d36a92
Liliya <lilyashushka@gmail.com>
2026-03-06 13:45:37 +0200

    [FIX] media carousel

--- src/components/CarouselBox.jsx
+++ src/components/CarouselBox.jsx
@@ -4,22 +4,22 @@ import Dog1 from "../assets/dog-1.jpg";
 import Dog2 from "../assets/dog-2.jpg";
 import Dog3 from "../assets/dog-3.jpg";
 import Dog4 from "../assets/dog-4.jpg";
-import { CarouselCaption } from "react-bootstrap";
+
 
 export default class CaruselBox extends React.Component {
     render() {
         return(
-          <Carousel>
+          <Carousel className="mt-5">
             <Carousel.Item>
                 <img
                 className="d-block w-100"
                 src={Dog1}
                 alt="Dog1"
                 />
-                <CarouselCaption>
+                <Carousel.Caption>
                 <h3>Dog 1</h3>
                 <p>First dog in the carousel</p>
-                </CarouselCaption>
+                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -27,10 +27,10 @@ export default class CaruselBox extends React.Component {
                 src={Dog2}
                 alt="Dog2"
                 />
-                 <CarouselCaption>
+                 <Carousel.Caption>
                 <h3>Dog 1</h3>
                 <p>First dog in the carousel</p>
-                </CarouselCaption>
+                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -38,10 +38,10 @@ export default class CaruselBox extends React.Component {
                 src={Dog3}
                 alt="Dog3"
                 />
-                 <CarouselCaption>
+                 <Carousel.Caption>
                 <h3>Dog 1</h3>
                 <p>First dog in the carousel</p>
-                </CarouselCaption>
+                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -49,10 +49,10 @@ export default class CaruselBox extends React.Component {
                 src={Dog4}
                 alt="Dog4"
                 />
-                 <CarouselCaption>
+                 <Carousel.Caption>
                 <h3>Dog 1</h3>
                 <p>First dog in the carousel</p>
-                </CarouselCaption>
+                </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
         )
