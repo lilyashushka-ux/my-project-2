ebb992f34e7690568d5218102362ba49bd938d31
Liliya <lilyashushka@gmail.com>
2026-03-07 12:08:41 +0200

    [FIX] contact block

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,30 @@
 import React from "react";
-import { Container, Form, Button, } from "react-bootstrap";
+import { Container, Form, Button, Card } from "react-bootstrap";
 
 export default class Contact extends React.Component {
     render() {
         return(
-         <Container style={{width:'500px', marginTop:'100px', }}>
+         <Container className="d-flex justify-content-center align-items-center vh-100" style={{width:'500px' }}>
+            <Card style={{ width: "450px", padding: "30px" }} className="shadow">
             <h1 className="text-center">Contact us</h1>
-            <Form>
-                <Form.Group contropId="formBasicEmail">
+            <Form className="mt-4">
+                <Form.Group className="mb-4" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
+                <Form.Text className="text-muted mt-2">
                 We'll never share your email with anyone else.
                 </Form.Text>
                 </Form.Group>
-                <Form.Group controlId="formBasicPassword">
+                <Form.Group className="mb-4" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control aria-rowspan={3} type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
-                  <Form.Group controlId="checkbox">
+                  <Form.Group className="mb-4" controlId="checkbox">
                 <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="dark" type="submit">Submit</Button>
+                <Button className="mb-4" variant="dark" type="submit">Submit</Button>
             </Form>
+            </Card>
          </Container>
     )
 }
